ddc00d08f74750167da7551a73e50967fc452cfb
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-09-27 14:41:01 -0500

    feat: :package: Add config and astro/node package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel'
-import sitemap from '@astrojs/sitemap'
-
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
+import node from "@astrojs/node";
 
 export default defineConfig({
   site: 'https://egidajuridica.com',
-  output: 'server',
-  adapter: vercel(),
+  output: 'server', // SSR
+  adapter: node({ mode: 'standalone' }),
   integrations: [sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      proxy: {
-        // Proxy para API local y evitar CORS en desarrollo
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
   },
 })
