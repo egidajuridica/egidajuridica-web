3f94955669e9dd5c506ab694f741499770dc1b23
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-10-07 22:41:35 +0200

    fix(config): 🔧 actualizar la URL del proxy de la API para usar el dominio correcto en el entorno de desarrollo y añadir el endpoint de autores

--- src/config/env/api.ts
+++ src/config/env/api.ts
@@ -4,7 +4,7 @@ export const API_CONFIG = {
   baseURL: import.meta.env.DEV
     ? isBrowser
       ? '/api'
-      : import.meta.env.PUBLIC_API_URL || 'https://app.egidajuridica.com/api'
+      : import.meta.env.PUBLIC_API_URL || 'https://egidajuridica.com/api'
     : import.meta.env.PUBLIC_API_URL || '/api',
   timeout: 10000,
   headers: {
@@ -12,12 +12,14 @@ export const API_CONFIG = {
   },
 } as const
 
+
 export const ENDPOINTS = {
   blog: '/blog',
   search: '/search-items',
   recursos: '/legal-resources',
   tags: '/tags',
   categories: '/categories',
+  authors: '/authors',
   blogTags: '/blogTags',
   resourceTags: '/resourceTags',
 } as const
